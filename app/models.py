34dedc542537c470340abfcec986cd4460d13ec4
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-11 02:12:38 +0900

    feat: Create Post models
    1. Post models 생성(ChannelLayerGroupSendMixin 상속, title과 content fields 생성, id 내림차순 정렬)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from app.mixins import ChannelLayerGroupSendMixin
+
 # Create your models here.
+class Post(ChannelLayerGroupSendMixin, models.Model):
+    CHANNEL_LAYER_GROUP_NAME = 'liveblog'
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-id']
