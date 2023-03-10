c4bd19ad8ff8b9be54520460584f9f064ca9fcf7
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-11 02:52:26 +0900

    feat: Create liveblog_index, post_partial view function
    1. liveblog_index를 통해서 post list를 liveblog_index.html을 통해서 랜더링
    2. post_partial를 통해서 지정 post id의 post를 조회 후 post_partial.html을 통해서 랜더링
    3. urlpatterns에 liveblog_index, post_partial path 매핑

--- app/views.py
+++ app/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from app.models import Post
 
 # Create your views here.
 def echo_apge(request):
-    return render(request, 'app/echo/container/page.html')
+    return render(request, 'app/container/echo/page.html')
+
+
+def liveblog_index(request):
+    post_qs = Post.objects.all()
+
+    return render(request, 'app/container/post/liveblog_index.html', {
+        'post_list': post_qs,
+    })
+
+
+def post_partial(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    return render(request, 'app/container/post/post_partial.html', {
+        'post': post,
+    })
