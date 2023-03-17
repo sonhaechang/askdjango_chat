f0e0e2c1da2b81b9d34c1f09a619525128aaacbd
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-17 21:53:07 +0900

    feat: WebSocket Connection (Create group chat as a one group)
    1. chat/consumers.py 생성(ChatConsumer class)
    2. chat/routing.py 생성(websocket_urlpatterns에 ChatConsumer path 매핑)
    3. asgi.py에 application/websocket/URLRouter에 'chat.routing.websocket_urlpatterns' 추가
    4. room_chat.html/handlers에 WebSokect 기능 추가

--- config/asgi.py
+++ config/asgi.py
@@ -13,6 +13,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import app.routing
+import chat.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
@@ -23,6 +24,7 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
     'websocket': URLRouter(
-        app.routing.websocket_urlpatterns
+        app.routing.websocket_urlpatterns +
+        chat.routing.websocket_urlpatterns
     ),
 })
