ce99a16353095415969dda6bc15e1557e4e46dcb
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-20 01:13:51 +0900

    feat: Create new room
    1. chat/views에 room_new 함수를 통해 room_new.html로 렌더링 (새로운 채팅방 생성 기능)
    2. chat/views에 room_chat 함수에 get_object_or_404 추가 및 room objects를 templates 변수로 넘김
    4. chat/urls/urlpatterns에 room_new 함수 path 매핑
    5. chat/forms/RoomForm 생성
    6. chat/templates/chat/container/room_new.html 생성
    7. chat/templates/chat/container/room_chat.html에 {{ room_name }} -> {{ romm.pk }}로 수정
    8. chat/templates/chat/container/index.html에 새 채팅방 만들기 a tag 추가

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from chat.forms import RoomForm
+from chat.models import Room
+
 
 # Create your views here.
 def index(request):
     return render(request, 'chat/container/index.html')
 
 
-def room_chat(request, room_name):
+def room_new(request):
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            new_room = form.save()
+            return redirect('chat:room_chat', new_room.pk)
+    else:
+        form = RoomForm()
+
+    return render(request, 'chat/container/room_new.html', {
+        'form': form,
+    })
+
+
+def room_chat(request, room_pk):
+    room = get_object_or_404(Room, pk=room_pk)
+    
     return render(request, 'chat/container/room_chat.html', {
-        'room_name': room_name,
+        'room': room,
     })
