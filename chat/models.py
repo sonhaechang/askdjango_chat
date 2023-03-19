b00f5a0c4790c0d19031120054884e83fa0f3987
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-20 01:10:25 +0900

    feat: Create Room models
    1. Room models 생성(채팅방 db)
    2. migrations files 생성 및 model migrate

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        editable=False
+    )
+
+    @property
+    def chat_group_name(self):
+        return self.make_chat_gourp_name(room=self)
+    
+    @classmethod
+    def make_chat_gourp_name(room=None, room_pk=None):
+        return 'chat-%s' % (room_pk or room.pk)
