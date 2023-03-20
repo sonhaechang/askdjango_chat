fbf72835288549d05ae35ba8cc50872e2394a00c
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-20 13:44:28 +0900

    feat: Create Login, Logout
    1. accounts/views에 login(LoginView)를 통해 login.html 렌더링 (로그인)
    2. accounts/views에 logout(LogoutView)를 통해 로그아웃 기능 구현
    3. accounts/urls/urlpattens에 login 함수 path 매핑
    4. accounts/urls/urlpattens에 logout 함수 path 매핑
    5. templates/accounts/container/login.html 생성
    6. tempaltes/components/navbar.html에 Login, Logout button -> a tag로 변경 및 href로 경로 추가
    7. templates/components/ base_form.html -> templates/components/base_form.html 이름 변경 및 {% load django_bootstrap5 %} 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 
+
 # Create your views here.
+login = LoginView.as_view(
+    template_name = 'accounts/container/login.html',
+    extra_context = {
+        'form_name': '로그인',
+        'submit_label': '로그인',
+    }
+)
+
+logout = LogoutView.as_view(
+    next_page = 'accounts:login'
+)
