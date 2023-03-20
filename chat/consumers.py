1e78ed960c4aae6c6c564cb3207de5a8bf9c72d1
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-20 15:31:03 +0900

    refector: Apply a different style to your messages
    1. 채팅 메시지 전달시 유저의 username을 추가로 전달(chat/consumers/ChatConsumer)
    2. 자신의 메지지일 경우, 클래스명 me 추가하기(chat/templates/chat/container/room_chat.html)

--- chat/consumers.py
+++ chat/consumers.py
@@ -33,15 +33,19 @@ class ChatConsumer(JsonWebsocketConsumer):
             )
 
     def receive_json(self, content, **kwargs):
+        user = self.scope['user']
         _type = content['type']
 
         if _type == 'chat.message':
             message = content['message']
+            sender = user.username
+            
             async_to_sync(self.channel_layer.group_send)(
                 self.group_name,
                 {
                     'type': 'chat.message',
                     'message': message,
+                    'sender': sender,
                 }
             )
         else:
@@ -51,6 +55,7 @@ class ChatConsumer(JsonWebsocketConsumer):
         self.send_json({
             'type': 'chat.message',
             'message': message_dict['message'],
+            'sender': message_dict['sender'],
         })
 
     
