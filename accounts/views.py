5fc1d45ed1c1723d5ac9bb7efdcab6fa5cf0eb2a
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-20 14:29:14 +0900

    feat: Create Sign up
    1. accounts/views에 signup(CreateView)를 통해 signup.html 렌더링(회원가입)
    2. accounts/urls/urlpattens에 signup 함수 path 매핑
    3. templates/accounts/container/signup.html 생성
    4. tempaltes/components/navbar.html에 Sign-up button -> a tag로 변경 및 href로 경로 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
 
 # Create your views here.
 login = LoginView.as_view(
     template_name = 'accounts/container/login.html',
     extra_context = {
-        'form_name': '로그인',
-        'submit_label': '로그인',
+        'form_name': 'Login',
+        'submit_label': 'Login',
     }
 )
+
 
 logout = LogoutView.as_view(
     next_page = 'accounts:login'
@@ -20,3 +23,14 @@ logout = LogoutView.as_view(
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/container/profile.html'
+
+
+signup = CreateView.as_view(
+    form_class = UserCreationForm,
+    template_name = 'accounts/container/signup.html',
+    extra_context = {
+        'form_name': 'Signup',
+        'submit_label': 'Signup',
+    },
+    success_url = reverse_lazy('accounts:login')
+)
