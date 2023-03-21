842306c6d80a9079a40b6ad0f73b5599fa68dd7f
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-21 16:53:11 +0900

    feat: Create RoomMemberAdmin
    1. chat.admin.py에 RoomMemberAdmin 생성

--- chat/admin.py
+++ chat/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from chat.models import Room
+from chat.models import Room, RoomMember
 
 
 # Register your models here.
@@ -8,3 +8,9 @@ from chat.models import Room
 class RoomAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'owner', 'created_at', 'updated_at']
     list_display_links = ['pk', 'name']
+
+
+@admin.register(RoomMember)
+class RoomMemberAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'room']
+    list_display_links = ['pk', 'user', 'room']
